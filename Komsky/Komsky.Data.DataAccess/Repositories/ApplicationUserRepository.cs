85fe872500333b73eeb7e96c538dd4b738e874b1
Michał Smyk <s.michalsmyk@gmail.com>
2016-01-04 12:56:01 +0100

    Unneccessary usings removed

--- Komsky/Komsky.Data.DataAccess/Repositories/ApplicationUserRepository.cs
+++ Komsky/Komsky.Data.DataAccess/Repositories/ApplicationUserRepository.cs
@@ -1,9 +1,5 @@
 ﻿using Komsky.Data.Entities;
-using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Data.Entity;
 
 namespace Komsky.Data.DataAccess.Repositories
